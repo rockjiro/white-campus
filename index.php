b75839b174fae62f1a5e3642664b59fb0b6914ac
ozato takumi <ozato@up-point.jp>
2018-11-28 16:41:46 +0900

    initialize commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>White Campus</title>
+	<link type="text/css" rel="stylesheet" href="css/reboot.min.css"/>
+	<link type="text/css" rel="stylesheet" href="css/style.css"/>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+<header class="header">
+	<div class="logo-wrapper">
+		<a href="#">
+			<img src="https://placehold.jp/300x200.png" alt="メインロゴ" class="header-logo">
+		</a>
+	</div>
+	<nav class="nav">
+		<ul>
+			<li><a href="#">about</a></li>
+			<li><a href="#">services</a></li>
+			<li><a href="#">contact</a></li>
+		</ul>
+	</nav>
+</header>
+<main>
+	<div class="main-visual">
+		<div class="bg"></div>
+		<div class="image">
+			<img src="https://placehold.jp/1200x700.png" alt="">
+		</div>
+	</div>
+</main>
+</body>
+</html>
